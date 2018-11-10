c8a6a87fb5965753f82efb0d80a97aaafa94feca
wassim93 <b.wassim1933@hotmail.fr>
2018-11-10 04:25:22 +0100

    adding pull to refresh layout

--- app/src/main/java/com/example/wassim/testapp/Activities/HomeActivity.java
+++ app/src/main/java/com/example/wassim/testapp/Activities/HomeActivity.java
@@ -21,6 +21,8 @@ import com.example.wassim.testapp.Services.Interfaces.ArticleService;
 import com.example.wassim.testapp.Utils.Listener.CustomItemClickListener;
 import com.facebook.shimmer.ShimmerFrameLayout;
 
+import org.json.JSONArray;
+
 import java.util.ArrayList;
 import java.util.List;
 
@@ -34,13 +36,10 @@ import static android.icu.lang.UCharacter.GraphemeClusterBreak.T;
 public class HomeActivity extends AppCompatActivity {
 
     protected ArticleListAdapter adapter;
-     protected RecyclerView recyclerView;
+    protected RecyclerView recyclerView;
     private ShimmerFrameLayout mShimmerViewContainer;
     private SwipeRefreshLayout swipeContainer;
     private android.support.v7.widget.Toolbar toolbar;
-
-
-
     final String ARTICLE_ID ="5729fc387fdea7e267fa9761";
 
     @Override
@@ -52,11 +51,18 @@ public class HomeActivity extends AppCompatActivity {
         getSupportActionBar().setTitle(null);
         mShimmerViewContainer = findViewById(R.id.shimmer_view_container);
         swipeContainer = findViewById(R.id.swipeContainer);
-
-
-
-
-
+        // Setup refresh listener which triggers new data loading
+        swipeContainer.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+                fetchTimelineAsync(0);
+            }
+        });
+        // Configure the refreshing colors
+        swipeContainer.setColorSchemeResources(android.R.color.holo_blue_bright,
+                android.R.color.holo_green_light,
+                android.R.color.holo_orange_light,
+                android.R.color.holo_red_light);
 
         /** Create handle for the RetrofitInstance interface*/
         ArticleService service = RetrofitInstance.getRetrofitInstance().create(ArticleService.class);
@@ -68,6 +74,15 @@ public class HomeActivity extends AppCompatActivity {
 
     }
 
+    public void fetchTimelineAsync(int page) {
+        ArticleService service = RetrofitInstance.getRetrofitInstance().create(ArticleService.class);
+
+        /** Call the method with parameter in the interface to get the notice data*/
+        Call<List<Article>> call = service.getArticles(ARTICLE_ID);
+        LoadData(call);
+
+    }
+
     private void LoadData(Call<List<Article>> call) {
         call.enqueue(new Callback<List<Article>>() {
             @Override
@@ -76,43 +91,38 @@ public class HomeActivity extends AppCompatActivity {
                 if (response.isSuccessful()) {
                     if (response.code() == 200) {
 
-
-
                         recyclerView = findViewById(R.id.list);
+                        if(adapter != null){
+                            adapter.clear();
+                            adapter.addAll(response.body());
+                            swipeContainer.setRefreshing(false);
+                        }else{
+                            adapter = new ArticleListAdapter(HomeActivity.this, (ArrayList<Article>) response.body(), new CustomItemClickListener() {
+                                @Override
+                                public void onItemClick(View v, int position) {
+                                    if(!response.body().get(position).getExternal_link().equals("")){
+                                        Intent i = new Intent(HomeActivity.this, DetailActivity.class);
+                                        i.putExtra("external_url", response.body().get(position).getExternal_link());
+                                        startActivity(i);
+                                    }else{
+                                        Toast.makeText(HomeActivity.this, "No external link found", Toast.LENGTH_SHORT).show();
 
-                        adapter = new ArticleListAdapter(HomeActivity.this, (ArrayList<Article>) response.body(), new CustomItemClickListener() {
-                            @Override
-                            public void onItemClick(View v, int position) {
-                                if(!response.body().get(position).getExternal_link().equals("")){
-                                    Intent i = new Intent(HomeActivity.this, DetailActivity.class);
-                                    i.putExtra("external_url", response.body().get(position).getExternal_link());
-                                    startActivity(i);
-                                }else{
-                                    Toast.makeText(HomeActivity.this, "No external link found", Toast.LENGTH_SHORT).show();
+                                    }
 
                                 }
+                            });
+                            RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(HomeActivity.this);
+                            recyclerView.setLayoutManager(layoutManager);
+                            recyclerView.setItemAnimator(new DefaultItemAnimator());
+                            recyclerView.setAdapter(adapter);
 
-                            }
-                        });
-                        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(HomeActivity.this);
-                        recyclerView.setLayoutManager(layoutManager);
-                        recyclerView.setItemAnimator(new DefaultItemAnimator());
-
-                        recyclerView.setAdapter(adapter);
-
-
-
+                        }
 
                     }
                     // stop animating Shimmer and hide the layout
-
                     mShimmerViewContainer.stopShimmer();
                     mShimmerViewContainer.setVisibility(View.GONE);
-
-
                 }
-
-
             }
 
             @Override
@@ -121,12 +131,6 @@ public class HomeActivity extends AppCompatActivity {
             }
         });
     }
-
-
-
-
-
-
 
     @Override
     public void onResume() {
